c694a51e5347b7862fd4456812a30df9789da113
OlenaBielova <belova.elena11@gmail.com>
2022-09-04 12:52:24 +0300

    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", handleSubmit); 
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    const elements = event.currentTarget.elements;
+    const email = elements.email.value;
+    const password = elements.password.value;
+    const formData = {
+        email, 
+        password,
+    }
+    console.log(formData);
+
+     if (email === "" || password === "" ) {
+        return alert("Please fill in all the fields!");
+    };
+
+    form.reset();
+    }
+
 
