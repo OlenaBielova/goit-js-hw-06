9cff2ea8866d74b440dfab67b340b5a93b4931ce
OlenaBielova <belova.elena11@gmail.com>
2022-09-06 11:50:06 +0300

    task10

--- js/task-09.js
+++ js/task-09.js
@@ -1,33 +1,13 @@
 const button = document.querySelector("button.change-color");
 const colorValue = document.querySelector(".color");
-const style = document.querySelector("style");
+const widget = document.querySelector(".widget");
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
 button.addEventListener("click", () => {
-style.innerHTML = 
-      `.widget {
-        text-align: center;
-        background-color: ${getRandomHexColor()};
-      }
-
-      .widget p {
-        margin-top: 0;
-        margin-bottom: 16px;
-        font-size: 48px;
-      }
-
-      .widget span {
-        font-family: monospace;
-      }
-
-      .widget button {
-        padding: 8px 12px;
-        font-size: 20px;
-        cursor: pointer;
-      }`
+widget.style.backgroundColor = `${getRandomHexColor()}`;
 
 colorValue.textContent = `${getRandomHexColor()}`;
     console.log(style.innerHTML);
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,53 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputNumber = document.querySelector("#controls input");
+const boxesContainer = document.querySelector("#boxes");
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+
+
+createBtn.addEventListener("click", () => {
+  const amount = Number(inputNumber.value);
+  // console.log(amount);
+
+  let arr = [];
+  let i = 0;
+  let x = 30;
+    while (i < amount) {
+      arr.push(x);
+      x += 10;
+      i += 1;
+    }
+  // console.log(arr);
+  
+  const boxesList = arr.map((x) => {
+    console.log(x);
+    const newDiv = document.createElement("div");
+    newDiv.style.width = `${x}px`;
+    newDiv.style.height = `${x}px`;
+    newDiv.style.backgroundColor = `${getRandomHexColor()}`;
+    return newDiv;
+  })
+  // console.log(boxesList);
+
+  boxesContainer.append(...boxesList);
+});
+// console.log(boxesContainer);
+  
+
+destroyBtn.addEventListener("click", () => {
+  boxesContainer.innerHTML = "";
+});
+
+
+
+
+
+
+
+
+// получить число из инпута
+//создать массив из Числа обектов - коробок - и map
+// insertAdjacentHTML 
